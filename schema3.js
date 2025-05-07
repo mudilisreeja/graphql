3328244702db36ed373451cf78d7fa8892743ab5
sreeja <sreeja.mudili@gmail.com>
2025-05-07 18:27:32 +0530

    schema for update mutation

---
+++ schema3.js
@@ -0,0 +1,44 @@
+export const typedefs = `#graphql
+  type Course {
+    id: ID!
+    title: String!
+    subjects: [Subject!]!  
+  }
+
+  type Subject {
+    id: ID!
+    name: String!
+    course: Course!
+    marks: [Mark!]!  
+  }
+
+  type Mark {
+    id: ID!
+    name: String!
+    value: Int
+    subject: Subject!
+  }
+
+  type Query {
+    courses: [Course!]!
+    course(id: ID!): Course
+    subjects: [Subject!]!
+    subject(id: ID!): Subject
+    marks: [Mark!]!
+    mark(id: ID!): Mark
+  }
+  type Mutation{
+    addCourse(course: AddCourseInput!): Course
+    deleteCourse(id:ID!): [Course]
+    updateCourse(id:ID!,course:UpdateCourseInput!): [Course]
+  }
+  input AddCourseInput {
+    title: String!
+  }
+  input UpdateCourseInput {
+    title: String
+}
+`;
+
+
+export default typedefs;
