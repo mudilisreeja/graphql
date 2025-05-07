073cded68884ec4c33a1d6a24a8d9a1f01c7b1da
sreeja <sreeja.mudili@gmail.com>
2025-05-07 17:20:02 +0530

    deletion of course with id

---
+++ deleteMutation.js
@@ -0,0 +1,60 @@
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+import typedefs from './schema1.js';
+import db from './db2.js';
+
+
+const resolvers = {
+  Query: {
+    courses(){
+        return db.courses
+    },
+    course(_,args){
+        return db1.courses.find((course)=>course.id===args.id)
+    },
+    subjects(){
+        return db.subjects
+    },
+    subject(_,args){
+        return db.subjects.find((subject)=>subject.id===args.id)
+    },
+    marks(){
+        return db.marks
+    },
+    mark(_,args){
+        return db.marks.find((mark)=>mark.id===args.id)
+    }
+    
+  },
+  Course: {
+    subjects(parent) {
+       return db.subjects.filter((subject) => subject.id === parent.subject_id);
+    }
+  },
+  Subject:{
+    marks(parent){
+        return db.marks.filter((m)=>m.subject_id===parent.id);
+    }
+  },
+ Mutation:{
+    deleteCourse(_,args){
+        db.courses=db.courses.filter((c)=>c.id !== args.id)
+        return db.courses
+    }
+
+ }
+
+  }
+
+
+const server = new ApolloServer({
+  typeDefs: typedefs,
+  resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
+
+console.log(`Server ready at ${url}`);
